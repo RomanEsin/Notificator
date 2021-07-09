ddbaeb8a8a28ef5b18d10e414b8fe0a954d02751
Roman Esin <esinromanswift@gmail.com>
2021-07-10 00:44:50 +1000

    Initial Commit

---
+++ Notificator/NotificatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificatorApp.swift
+//  Notificator
+//
+//  Created by Роман Есин on 10.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotificatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
